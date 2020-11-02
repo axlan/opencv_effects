2063612455c931d32ce26838af33b8969b7c1f79
Jonathan Diamond <feros32@gmail.com>
2020-11-02 13:22:01 -0800

    Clean up and comment code

--- effect_demo.py
+++ effect_demo.py
@@ -1,15 +1,20 @@
 import cv2
-import sys
 import numpy as np
+
+import sys
 import glob
 
+
+# Rotate an image around its center
+# angle is given in degrees
 def rotate_image(image, angle):
   image_center = tuple(np.array(image.shape[1::-1]) / 2)
   rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
   result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
   return result
 
-
+# Draw the image top over the image bottom, offset by x,y
+# Handles transparency in the top image
 def draw_on_image(bottom, top, x=0, y=0):
     (h, w) = top.shape[:2]
     y1, y2 = y, y + h
@@ -34,104 +39,136 @@ def draw_on_image(bottom, top, x=0, y=0):
     if bottom.shape[2] == 4:
         bottom[y1:y2, x1:x2, 3] = np.maximum(top[:h, :w, 3], bottom[y1:y2, x1:x2, 3])
 
-def main():
-    cascPath = 'haarcascade_frontalface_default.xml'
-    faceCascade = cv2.CascadeClassifier(cascPath)
+# Draw an animated laughing man effect over faces detected in a frame
+class LaughingFaceEffect:
+    ROTATION_RATE = -2.0
+    FACE_IMAGE_PATH = "images/face.png"
+    TEXT_IMAGE_PATH = "images/text_centered.png"
+    CASCADE_XML_PATH = 'haarcascade_frontalface_default.xml'
+    
+    def __init__(self):
+        self.rotation = 0.0
+        self.face_img = cv2.imread(LaughingFaceEffect.FACE_IMAGE_PATH, -1)
+        self.text_img = cv2.imread(LaughingFaceEffect.TEXT_IMAGE_PATH, -1)
+        self.faceCascade = cv2.CascadeClassifier(LaughingFaceEffect.CASCADE_XML_PATH)
 
-    video_capture = cv2.VideoCapture(0)
-
-    video_capture.set(3,1280)
-    video_capture.set(4,720)
-
-    rotation = 0.0
-    rotation_rate = -2.0
-    face_img = cv2.imread("images/face.png", -1)
-    text_img = cv2.imread("images/text_centered.png", -1)
-
-    magic_frame_index = 0
-    magic_frame_files = glob.glob('images/magic_circle_frames/magic*')
-    magic_frames = [cv2.imread(filename) for filename in magic_frame_files]
-
-    #Load the dictionary that was used to generate the markers.
-    dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-
-    # Initialize the detector parameters using default values
-    parameters =  cv2.aruco.DetectorParameters_create()
-
-
-    while True:
-        # Capture frame-by-frame
-        ret, frame = video_capture.read()
-
+    def process_frame(self, frame):
+        
+        # get greyscale frame
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        faces = faceCascade.detectMultiScale(
+        # run face detection
+        faces = self.faceCascade.detectMultiScale(
             gray,
             scaleFactor=1.2,
             minNeighbors=5,
             minSize=(50, 50),
             flags=cv2.CASCADE_SCALE_IMAGE
         )
-
-        # Draw a rectangle around the faces
+        # draw laughing man on each face
         for (x, y, w, h) in faces:
+            # Scale image to be larger then detected face
             w = int(w * 1.2)
             h = int(h * 1.2)
 
-            combined = np.zeros(face_img.shape)
-
-            rotated = rotate_image(text_img, rotation)
-
+            # Create empty image to draw on
+            combined = np.zeros(self.face_img.shape)
+            # Draw on rotated text
+            rotated = rotate_image(self.text_img, self.rotation)
             draw_on_image(combined, rotated, 22, 26)
-            draw_on_image(combined, face_img)
+            # Draw on face template
+            draw_on_image(combined, self.face_img)
+            # Resize to detected face
             face_resized = cv2.resize(combined, (w, h))
-
+            # Draw on original frame
             draw_on_image(frame, face_resized, x, y)
-        rotation += rotation_rate
+        self.rotation += LaughingFaceEffect.ROTATION_RATE
 
-        if True:
-            # for simple noise reduction we use deque
-            from collections import deque
-            # simple noise reduction
-            h_array = deque(maxlen = 5)
+# Draw an animated magic circle over ArUco markers in frame
+class MagicCircleEffect:
+    ROTATION_RATE = -2.0
+    CIRLCE_FRAMES_PATH = 'images/magic_circle_frames/magic*'
+    
+    def __init__(self):
+        self.magic_frame_index = 0
+        # Load animation frames from path
+        magic_frame_files = glob.glob(MagicCircleEffect.CIRLCE_FRAMES_PATH)
+        self.magic_frames = [cv2.imread(filename) for filename in magic_frame_files]
+        #Load the dictionary that was used to generate the markers.
+        self.dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+        # Initialize the detector parameters using default values
+        self.parameters =  cv2.aruco.DetectorParameters_create()
 
-           
+    def process_frame(self, frame):
+        
         # Detect the markers in the image
-        markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)
+        markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame,
+            self.dictionary,
+            parameters=self.parameters)
 
+        # Draw image over each marker
         for corners in markerCorners:
-
-            im_src = magic_frames[magic_frame_index]
+            # Current frame of circle animation 
+            im_src = self.magic_frames[self.magic_frame_index]
             (h, w) = im_src.shape[:2]
 
+            # Find transform between source image and marker in frame
             pts_dst = np.concatenate(corners, axis = 1)
             pts_src = np.float32([[0, 0], [0, w], [h , w], [h, 0]])
-
-
-            RES_SIZE = (frame.shape[1], frame.shape[0])
-
             M = cv2.getPerspectiveTransform(pts_src, pts_dst)
 
+            RES_SIZE = (frame.shape[1], frame.shape[0])
+            # create a projection of im_src with a white background
             warped = cv2.warpPerspective(im_src, M, RES_SIZE, cv2.INTER_LINEAR, borderValue=(255, 255, 255))
+            # Add an alpha channel and make white points transparent
             warped = cv2.cvtColor(warped, cv2.COLOR_RGB2RGBA)
-            warped[warped[:,:,0] == 255] = 0
+            warped[(warped[:,:,0] == 255) & (warped[:,:,1] == 255) & (warped[:,:,2] == 255)] = 0
 
+            # Draw on original frame
             draw_on_image(frame, warped)
             
+        self.magic_frame_index += 1
+        self.magic_frame_index %= len(self.magic_frames)
 
-        magic_frame_index += 1
-        magic_frame_index %= len(magic_frames)
+def main():
 
+    show_face_effect = False
+    show_circle_effect = False
 
+    # Initialize camera
+    video_capture = cv2.VideoCapture(0)
+    video_capture.set(3,1280)
+    video_capture.set(4,720)
+
+    laughing_face_effect = LaughingFaceEffect()
+    magic_circle_effect = MagicCircleEffect()
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        if show_face_effect:
+            laughing_face_effect.process_frame(frame)
+        if show_circle_effect:
+            magic_circle_effect.process_frame(frame)
 
         # Display the resulting frame
         cv2.imshow('Video', frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        # Check for key presses
+        key_press = cv2.waitKey(1) & 0xFF
+        # Exit on 'q'
+        if key_press == ord('q'):
             break
+        # toggle show_face_effect on 'f'
+        elif key_press == ord('f'):
+            show_face_effect = not show_face_effect
+        # toggle show_circle_effect on 'm'
+        elif key_press == ord('m'):
+            show_circle_effect = not show_circle_effect
 
     # When everything is done, release the capture
     video_capture.release()
     cv2.destroyAllWindows()
 
-main()
+if __name__ == "__main__":
+    main()
